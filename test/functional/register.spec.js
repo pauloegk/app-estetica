21734cbbce1e2f18a64bdd4821e8bde64570f028
Paulo Eduardo Groth <pauloegk@MacBook-Air-de-Paulo.local>
2019-09-30 07:15:52 -0400

    first commit

---
+++ test/functional/register.spec.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const { test, trait } = use('Test/Suite')('Register')
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use('App/Models/User')
+
+trait('Test/ApiClient')
+
+test('register user valid', async ({ client }) => {
+  await User.create({
+    username: 'Pauloegk',
+    email: 'pauloegk@gmail.com',
+    password: '1234567'
+  })
+
+  const response = await client.get('/posts').end()
+  response.assertStatus(200)
+});
+
+test('register user invalid e-mail', async ({ client }) => {
+  await User.create({
+    username: 'Pauloegk',
+    email: 'pauloegk',
+    password: '1234567'
+  })
+
+  const response = await client.get('/posts').end()
+  response.assertStatus(404)
+});
+
